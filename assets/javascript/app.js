6b66273c521490c66ee75807d46dd0807c2b2c4c
bwillstorey <bwillstorey@gmail.com>
2019-02-24 12:36:33 -0600

    built start button in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,40 @@
-$(document).ready()
+var questions {
+    q1: Where
+}
+
+var images = ["../assets/images/coffee-can.gif", "../assets/images/donny.gif", "../assets/images/dude-dance.gif", "../assets/images/flea.gif", "../assets/images/jesus.gif", "../assets/images/larry.gif", "../assets/images/rug.gif", "../assets/images/sam-elliott.gif", "../assets/images/white-russian.gif"];
+
+var startButton = $("<button class ='start'>Let's Go Bowling</button>");
+
+$(document).ready(function(){
+
+    $("#start-page").append(startButton);
+
+    $(document).on("click", ".start", playGame);
+
+
+
+
+
+
+})
+
+// Starts the game
+function playGame () {
+    
+    if ()
+}
+
+// Load question 1, answer options for q1, and start timer after start button clicked
+
+// Remove page content, load answer page for q1 with image displayed
+
+// Remove answer content, load q2
+
+
+
+
+
+
+
+
