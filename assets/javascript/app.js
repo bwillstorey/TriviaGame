fbc4aa9ae7229a39012b6066d17aa89fd6bc1ecc
bwillstorey <bwillstorey@gmail.com>
2019-02-24 15:47:06 -0600

    updated objects with images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,51 +3,63 @@ var questions = [
     {
     quest: "What is the name of the main character in 'The Big Lebowski'?",
     choices: ["The Man", "The Boss", "The Dude"],
-    answer: "The Dude"
+    answer: "The Dude",
+    imgUrl: "../images/dude-dance.gif"
     },
     {
     quest: "What is the Dude's favorite beverage?",
     choices: ["White Russian", "Manhattan", "Mint Julep"],
-    answer: "White Russian"
+    answer: "White Russian",
+    imgUrl: "../images/white-russian.gif"
     },
     {
     quest: "Who narrates 'The Big Lebowski?",
     choices: ["Morgan Freeman", "Sam Elliott", "Gary Busey"],
-    answer: "Sam Elliott"
+    answer: "Sam Elliott",
+    imgUrl: "../images/sam-elliott.gif"
     },
     {
     quest: "What really ties the room together?",
     choices: ["good lighting", "a rug", "a couch"],
-    answer: "a rug"
+    answer: "a rug",
+    imgUrl: "../images/rug.gif"
     },
     {
     quest: "What is the name of the Dude and Walter's friend?",
     choices: ["Donny", "Jackie", "Arthur"],
-    answer: "Donny"
+    answer: "Donny",
+    imgUrl: "../images/donny.gif"
     },
     {
     quest: "What famous musician makes a cameo in the movie?",
     choices: ["Marky Mark", "Dave Grohl", "Flea"],
-    answer: "Flea"
+    answer: "Flea",
+    imgUrl: "../images/flea.gif"
     },
     {
     quest: "Who do the Dude and Walter accuse of stealing the car?",
     choices: ["Johnny", "Larry", "Harry"],
-    answer: "Larry"
+    answer: "Larry",
+    imgUrl: "../images/larry.gif"
     },
     {
     quest: "Who do you not mess with?",
     choices: ["Malibu Police", "Smokey", "The Jesus"],
-    answer: "The Jesus"
+    answer: "The Jesus",
+    imgUrl: "../images/jesus.gif"
+    },
+    {
+    quest: "What does Walter use for Donny's urn?",
+    choices: ["bucket", "coffee can", "carboard box"],
+    answer: "coffee can",
+    imgUrl: "../images/coffee-can.gif"
     }
 ]
-// array of gifs to be display on the answer pages
-var images = ["../assets/images/coffee-can.gif", "../assets/images/donny.gif", "../assets/images/dude-dance.gif", "../assets/images/flea.gif", "../assets/images/jesus.gif", "../assets/images/larry.gif", "../assets/images/rug.gif", "../assets/images/sam-elliott.gif", "../assets/images/white-russian.gif"];
 
 // a button to start the game
 var startButton = $("<button class ='start'>Let's Go Bowling</button>");
 
-
+var answerChosen;
 
 $(document).ready(function(){
 
@@ -74,10 +86,12 @@ setInterval(function () {
     }
     }, 1000);
 
-// TIMEOUT
-function timeout () {
+// wait for user input or timeout
+answerChosen = $(this).text(answer[i]);
 
-}
+
+// function userInput () {
+
 
 // STARTS THE GAME
 function playGame () {
@@ -99,19 +113,34 @@ function playGame () {
             $(".ans-btn").text(currentAnswers);
 
         }
-       
-        setTimeout(timeout, 1000 * counter);
+        $(".ans-btn").on("click", function(){
+            if (answerChosen === answer[i]) {
+                showAnswer()
+                $("#yes-no").text("The Dude Abides");
+                $("#correct-answer").text(answer[i]);
+                $(".answer-image").attr("src", "")
+            }
+            else if (answerChosen !== answer[i]) {
+                
+            }
+        
+        })
+        setTimeout(1000 * 20);
+
+
         
     }    
 
 }
 
 
-// Remove page content, load answer page for q1 with image displayed
-$(".ans-btn").on("click", function(){
+// move to answer page
+function showAnswer () {
     $("#question-page").hide();
     $("#answer-page").show();
-})
+
+    
+}
 
 
 // Remove answer content, load q2
