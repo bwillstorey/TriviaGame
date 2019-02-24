b73c696504fa4f1d3a4bd4d83cc524258ec67155
bwillstorey <bwillstorey@gmail.com>
2019-02-24 13:02:40 -0600

    building out questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,42 @@
-var questions {
-    q1: Where
-}
+var questions = [ 
+    {
+    q1: "What is the name of the main character in 'The Big Lebowski'?",
+        a1: {
+            a: "The Man",
+            b: "The Boss",
+            c: "The Dude"
+         },
+    answer1: "The Dude"
+    },
+    {
+    q2: "What is the Dude's favorite beverage?",
+        a2: {
+            a: "White Russian",
+            b: "Manhattan",
+            c: "Mint Julep"
+        },
+    answer2: "White Russian"
+    },
+    {
+    q3: "Who narrates 'The Big Lebowski?",
+        a3: {
+            a: "Morgan Freeman",
+            b: "Sam Elliott",
+            c: "Gary Busey"
+        },
+    answer3: "Sam Elliott"
+    },
+    {
+    q4: "What really ties the room together?",
+        a4: {
+
+        },
+    },
+    q5: "What is the name of the Dude and Walter's friend?",
+    q6: "What famous musician makes a cameo in the movie?",
+    q7: "Who do the Dude and Walter accuse of stealing the car?",
+    q8: "Who do you not mess with?"
+]
 
 var images = ["../assets/images/coffee-can.gif", "../assets/images/donny.gif", "../assets/images/dude-dance.gif", "../assets/images/flea.gif", "../assets/images/jesus.gif", "../assets/images/larry.gif", "../assets/images/rug.gif", "../assets/images/sam-elliott.gif", "../assets/images/white-russian.gif"];
 
@@ -21,11 +57,31 @@ $(document).ready(function(){
 
 // Starts the game
 function playGame () {
+
+// Remove start page content
+    $("#start-page").hide();
+
+    // Load question 1
+    for (var i = 0; i < questions.length; i++) {
+        var currentQuestion = questions[i];
+        $("#question").text(currentQuestion);
+
+        // load answer options for q1
+
+
+    }
+
     
-    if ()
+    // answer options for q1 
+    
+    // start timer
+
+
+    
+
 }
 
-// Load question 1, answer options for q1, and start timer after start button clicked
+
 
 // Remove page content, load answer page for q1 with image displayed
 
